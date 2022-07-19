cec8ba91c20fb668f1249ee233d56a6c441a4c3c
Guilherme Beutler <guilherme.beutler@outlook.com>
2022-07-19 17:43:03 -0300

    ajustes

--- pokedex8/src/Pages/Home/Home.js
+++ pokedex8/src/Pages/Home/Home.js
@@ -1,29 +1,5 @@
 
 import React, { useEffect, useState } from "react";
-// import axios from 'axios'
-
-export default function Home() {
-
-    // const pokemonsList = [pokemonList, setPokemonList] = useState('')
-
-    // const getPokemons = () => {
-    //     axios
-    //         .get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0')
-    //         .then((response) => {
-    //             setPokemonList(response.data.results)
-    //         })
-    //         .catch((error) => {
-    //             console.log(error.data.message)
-    //         })
-    // }
-
-    // useEffect(() => {
-    //     requests.getPokemons()
-    // }, [])
-
-
-
-
 import React, { useContext } from "react";
 import Header from "../../Components/Header";
 import { PokemonContext } from "../../Context/PokemonContext";
